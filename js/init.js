8991941606a35fe429faec24be42d66d0ce27734
Jose Ignacio Dominguez Sanchez <ignacio.dominguez@compucom.com>
2021-05-26 14:12:50 -0500

    tercer commit firebase

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+	apiKey: "AIzaSyDG9E0qtX_QQeGwkolqc1_99TblNItIZ5A",
+    authDomain: "fbwebauth3nv41.firebaseapp.com",
+    projectId: "fbwebauth3nv41",
+    storageBucket: "fbwebauth3nv41.appspot.com",
+    messagingSenderId: "486850033174",
+    appId: "1:486850033174:web:9443b624c1ff03bfb4a737",
+    measurementId: "G-L3GG8VZJL9"
+  });
